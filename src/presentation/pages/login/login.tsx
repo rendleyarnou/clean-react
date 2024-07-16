954d8ade1a73c314068752d603de138e22814ae4
Rendley Arnou Xavier <rendley.xavier@sps.ce.gov.br>
2024-07-16 09:13:34 -0300

    feat: add login view

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Styles from './login-styles.scss';
+import Spinner from '@/presentation/components/spinner/spinner';
 
 const Login: React.FC = () => {
   return (
@@ -11,10 +12,22 @@ const Login: React.FC = () => {
       <form className={Styles.form}>
         <h2>Login</h2>
 
-        <input type="email" name='email' placeholder='Digite seu e-mail' />
-        <input type="password" name='password' placeholder='Digite sua senha' />
+        <div className={Styles.inputWrap}>
+          <input type="email" name='email' placeholder='Digite seu e-mail' />
+          <span className={Styles.status}>🔴</span>
+        </div>
+        <div className={Styles.inputWrap}>
+          <input type="password" name='password' placeholder='Digite sua senha' />
+          <span className={Styles.status}>🔴</span>
+        </div>
 
-        <button type="submit">Entrar</button>
+        <button type="submit" className={Styles.submit}>Entrar</button>
+        <span className={Styles.link}>Criar conta</span>
+
+        <div className={Styles.errorWrap}>
+          <Spinner className={Styles.spinner}/>
+          <span className={Styles.error}>Erro</span>
+        </div>
       </form>
       <footer className={Styles.footer} />
     </div>
