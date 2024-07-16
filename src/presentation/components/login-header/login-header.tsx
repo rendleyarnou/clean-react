c1c3b7cf6bc44b89eb50358963904da60133fb10
Rendley Arnou Xavier <rendley.xavier@sps.ce.gov.br>
2024-07-16 09:36:59 -0300

    refactor: move header and footer to separate files

---
+++ src/presentation/components/login-header/login-header.tsx
@@ -0,0 +1,14 @@
+import React, { memo } from 'react'
+import Styles from './login-header-styles.scss';
+import Logo from '../logo/logo';
+
+const LoginHeader: React.FC = () => {
+  return (
+    <header className={Styles.header}>
+      <Logo />
+      <h1>ReactJs com Webpack, Clean Architecture e Clean Code</h1>
+    </header>
+  )
+}
+
+export default memo(LoginHeader)
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import Styles from './login-styles.scss';
 import Spinner from '@/presentation/components/spinner/spinner';
-import Logo from '@/presentation/components/logo/logo';
+import LoginHeader from '@/presentation/components/login-header/login-header';
+import Footer from '@/presentation/components/footer/footer';
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-      <header className={Styles.header}>
-        <Logo />
-        <h1>ReactJs com Webpack, Clean Architecture e Clean Code</h1>
-      </header>
+      <LoginHeader />
       <form className={Styles.form}>
         <h2>Login</h2>
 
@@ -30,7 +28,7 @@ const Login: React.FC = () => {
           <span className={Styles.error}>Erro</span>
         </div>
       </form>
-      <footer className={Styles.footer} />
+      <Footer />
     </div>
   )
 }
