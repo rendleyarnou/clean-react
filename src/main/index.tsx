5c5ec474bbbbcf449405f14325ca3aa89e756947
Rendley Arnou Xavier <rendley.xavier@sps.ce.gov.br>
2024-07-16 13:17:18 -0300

    feat: add app router

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,7 @@
+import { Router } from '@/presentation/components';
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { Login } from '@/presentation/pages'
 
 const root = createRoot(document.getElementById('main'));
 
-root.render(<Login />)
-
+root.render(<Router />)
