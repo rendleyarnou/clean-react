fe3574ffe7f8ebd75f0dfd1f83cefef62a47a5e1
Rendley Arnou Xavier <rendley.xavier@sps.ce.gov.br>
2024-07-17 09:21:40 -0300

    fix: move global styles to index

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,7 @@
 import { Router } from '@/presentation/components';
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import '@/presentation/styles/global.scss'
 
 const root = createRoot(document.getElementById('main'));
 
