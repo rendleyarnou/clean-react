31ebcf630abc5767a1e8864a498c60d75433c26b
Rendley Xavier <rendley.xavier@sps.ce.gov.br>
2024-09-18 14:40:15 -0300

    feat: ensure RemoteAddAccount throws UnexpectedError if HttpPostClient returns 400

--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -1,7 +1,7 @@
 import type { AddAccount, AddAccountParams } from '@/domain/usecases'
 import type { AccountModel } from '@/domain/models'
 import { HttpStatusCode, type HttpPostClient } from '@/data/protocols/http'
-import { EmailInUseError } from '@/domain/errors'
+import { EmailInUseError, UnexpectedError } from '@/domain/errors'
 
 export class RemoteAddAccount implements AddAccount {
   constructor(
@@ -18,10 +18,12 @@ export class RemoteAddAccount implements AddAccount {
       body: params
     })
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        return null
       case HttpStatusCode.forbidden:
         throw new EmailInUseError()
       default:
-        return null
+        throw new UnexpectedError()
     }
   }
 }
