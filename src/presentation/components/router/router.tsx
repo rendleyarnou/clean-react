ced806d20cefc25b8a694a13d225ebe976c35d2c
Rendley Xavier <rendley.xavier@sps.ce.gov.br>
2024-09-19 10:34:58 -0300

    feat: ensure Signup change name status to error if validations fails

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -11,7 +11,7 @@ const Router: React.FC<Props> = ({ MakeLogin }: Props) => {
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={<MakeLogin />} />
-        <Route path='/signup' element={<Signup />} />
+        <Route path='/signup' element={<Signup validation={undefined} />} />
       </Routes>
     </BrowserRouter>
   )
