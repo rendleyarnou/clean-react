713d19eeee4697ef9a539a25e1a8fc212883d336
Rendley Xavier <rendley.xavier@sps.ce.gov.br>
2024-11-04 16:48:47 -0300

    chore: adiciona commands do cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080',
     fixturesFolder: false,
-    supportFile: false,
+    supportFile: 'src/main/test/cypress/support/commands.ts',
     specPattern: 'src/main/test/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
